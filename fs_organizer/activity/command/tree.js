e5a7edc6b1841989b19f245819f988e8ed468f27
AshwajitSingh <64692851+AshwajitSingh@users.noreply.github.com>
2021-08-10 06:51:19 +0530

    Add files via upload

--- fs_organizer/activity/command/tree.js
+++ fs_organizer/activity/command/tree.js
@@ -1,8 +1,22 @@
-function treeFn(src) {
-    console.log("tree command executed with path : " + src);
+let fs = require("fs");
+let path = require("path");
+
+function treeFn(src){
+    if(src==undefined){
+        src = process.cwd();
+
+    }
+        let content = fs.readdirSync(src);
+        let parentFolder = path.basename(src);
+        let completePath = "|____"+ parentFolder;
+        for(let i=0;i<content.length;i++){
+            completePath=completePath+"\n\t"+"|---"+content[i];
+
+        }
+        console.log(completePath);
     
 }
 
-module.exports = {
-    treefxn: treeFn
+module.exports ={
+    treeFxn:treeFn,
 }
